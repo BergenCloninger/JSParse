defc558ea450451d54edf9c16c3242e8676f8013
BergenCloninger <105687249+BergenCloninger@users.noreply.github.com>
2025-01-23 10:51:39 -0700

    1/23/25 In class

--- evaluator.js
+++ evaluator.js
@@ -1,10 +1,29 @@
-class Evaluator{
+class Evaluator {
     checkNodeType(node) {
         return node.type === 'VariableDeclaration';
     }
     
     readVarName(node) {
         return node.name;
+    }
+
+    makeVarPair(varName) {
+        return {
+            "name": varName
+        }
+    }
+
+    readNodeVal(node) {
+        return node.value;
+    }
+
+    makeValuePair(varValue) {
+        return {"value": varValue};
+    }
+
+    storeEntryInEnvironment(varNamePair, valuePair, environment) {
+        environment.push({...varNamePair, ...valuePair});
+        return environment;
     }
 }
 module.exports=Evaluator;
--- test/eval.test.js
+++ test/eval.test.js
@@ -1,10 +1,11 @@
 const Getnodes = require("./../getnodes");
 const Evaluator = require("./../evaluator");
+
+const evaluator = new Evaluator();
 let body ;
 
 beforeAll( ()=>{
     const nodes = new Getnodes();
-
     const loaded = nodes.loadFile("./test/sample.js")
     body = nodes.parseFile(loaded);
 
@@ -20,8 +21,6 @@ test('two nodes parsed ', ()=>{
 })
 
 test('check if node has type: \'VariableDeclaration\':', ()=>{
-    const evaluator = new Evaluator(); //should this be global? 
-
     const node1 = evaluator.checkNodeType(body[0]); //check first node
     const node2 = evaluator.checkNodeType(body[1]); //check second node
     expect(node1).toBe(true);
@@ -29,6 +28,30 @@ test('check if node has type: \'VariableDeclaration\':', ()=>{
 })
 
 test('variable name is a', ()=>{
-    const evaluator = new Evaluator();
     expect(evaluator.readVarName(body[0].declarations[0].id)).toBe('a');
+})
+
+test('given string a, make a varPair with it', ()=>{
+    const varName = 'a';
+    const result = evaluator.makeVarPair(varName);
+
+    expect(result).toStrictEqual({"name": "a"})
+})
+
+test('given varDec node read node value', ()=>{
+    expect(evaluator.readNodeVal(body[0].declarations[0].init)).toBe('hello');
+})
+
+test("given VarValue, make a valuePair with it", ()=>{
+    const varValue = "hello";
+    const result = evaluator.makeValuePair(varValue);
+    expect(result).toStrictEqual({"value": "hello"});
+})
+
+test("given varName, varValue, and environment, combine and store", ()=> {
+    const valNamePair = {"name": 'a'};
+    const valuePair = {"value": "hello"};
+    const environment = [];
+    const result = evaluator.storeEntryInEnvironment(valNamePair, valuePair, environment);
+    expect(result).toStrictEqual([{"name": 'a', "value": "hello"}]);
 })
